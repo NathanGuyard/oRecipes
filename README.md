b25cb933da3dea83a257eb6d2175835f34f1f632
NathanGuyard <nathan.arkanoxd12@gmail.com>
2021-10-20 16:42:05 +0200

    Projet O'recipes

---
+++ README.md
@@ -0,0 +1,14 @@
+# oRecipes
+
+Site de recette fait en React, Redux et React Router
+
+Grâce à l'aide de Axios je reçois toutes les recettes via une API en local 
+
+
+## Aperçus du site 
+
+!['résultat'](./resultat.png)
+
+## Aperçus du site avec quelque animations
+
+!['résultat'](./resultat.gif)
---
+++ src/components/AppHeader/index.js
@@ -0,0 +1,50 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+import './style.scss';
+import logo from 'src/assets/logo.png';
+import LoginForm from 'src/components/LoginForm';
+
+const AppHeader = () => {
+  const email = useSelector((state) => state.user.email);
+  const password = useSelector((state) => state.user.password);
+  const logged = useSelector((state) => state.user.logged);
+  const pseudo = useSelector((state) => state.user.pseudo);
+  const dispatch = useDispatch();
+  const changeField = (value, key) => {
+    dispatch({
+      type: 'CHANGE_VALUE',
+      value: value,
+      key: key,
+      // en es6 quand on met une propriété qui a pour valeur
+      // la valeur d'une variable possédant le meme nom que la clé on peut abréger
+      // value,
+      // key,
+    });
+  };
+  const login = () => {
+    dispatch({
+      type: 'LOGIN',
+    });
+  };
+  const logout = () => {
+    dispatch({
+      type: 'LOGOUT',
+    });
+  };
+  return (
+    <header className="header">
+      <img src={logo} className="header-logo" alt="Logo oRecipes" />
+      <LoginForm
+        email={email}
+        password={password}
+        changeField={changeField}
+        handleLogin={login}
+        isLogged={logged}
+        loggedMessage={`Connecté en tant que ${pseudo}`}
+        handleLogout={logout}
+      />
+    </header>
+  );
+};
+
+export default AppHeader;
